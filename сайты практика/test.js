c7608ebdf0fda38a76f24c98f29366da557fd124
niki69ta <73909069+niki69ta@users.noreply.github.com>
2021-08-30 18:43:12 +0300

    Add files via upload

---
+++ сайты практика/test.js
@@ -0,0 +1,10 @@
+function getAverage(a,b) {
+
+  var average = (a + b) / 2;
+  console.log(average);
+  return average;
+
+}
+
+var myResult = getAverage(7,8);
+console.log('the result is ' + myResult)
